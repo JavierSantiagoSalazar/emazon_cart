361e129cb88ab5b8abf71e8794f98518121a0228
Javier Santiago <javiersantiago45011@gmail.com>
2024-09-12 21:29:11 -0500

    Authorization Validation Endpoints for Cart Microservice:
    - Created mock endpoints for testing authorization functionality in cart microservice
    - Implemented JWT-based authentication and role-based access control for cart endpoints
    - Configured security settings in application.properties for cart microservice
    - Updated SecurityConfiguration and JwtUtils for authorization validation in cart microservice
    - Added CustomAuthenticationEntryPoint for handling unauthorized access to cart endpoints
    
    This commit adds mock endpoints for testing authorization functionality in the cart microservice and configures security settings for validation.

--- src/main/java/com/pragma/emazon_cart/infrastructure/utils/JwtUtils.java
+++ src/main/java/com/pragma/emazon_cart/infrastructure/utils/JwtUtils.java
@@ -6,7 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.pragma.emazon_transaction.domain.utils.Constants;
+import com.pragma.emazon_cart.domain.utils.Constants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
